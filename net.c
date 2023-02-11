ca652e50392502e33b10a4b19a41931ad03caf8d
t0mmy <tommycsth61@gmail.com>
2023-02-12 00:55:14 +0900

    NIC のオープン・クローズを実装

--- net.c
+++ net.c
@@ -30,3 +30,40 @@ int net_device_register(struct net_device *dev) {
   infof("registered, dev=%s, type=0x%04x", dev->name, dev->type);
   return 0;
 }
+
+static int net_device_open(struct net_device *dev) {
+  if (NET_DEVICE_IS_UP(dev)) {
+    // すでに UP 状態なのでエラーとする
+    errorf("already opened, dev=%s", dev->name);
+    return -1;
+  }
+
+  if (dev->ops->open) {
+    if (dev->ops->close(dev) == -1) {
+      errorf("failure, dev=%s", dev->name);
+      return -1;
+    }
+  }
+
+  dev->flags |= NET_DEVICE_FLAG_UP;
+  infof("dev=%s, state=%s", dev->name, NET_DEVICE_STATE(dev));
+  return 0;
+}
+
+static int net_device_close(struct net_device *dev) {
+  if (!NET_DEVICE_IS_UP(dev)) {
+    errorf("not opened, dev=%s", dev->name);
+    return -1;
+  }
+
+  if (dev->ops->close) {
+    if (dev->ops->close(dev) == -1) {
+      errorf("failure, dev=%s", dev->name);
+      return -1;
+    }
+  }
+
+  dev->flags &= ~NET_DEVICE_FLAG_UP;
+  infof("dev=%s, state=%s", dev->name, NET_DEVICE_STATE(dev));
+  return 0;
+}
